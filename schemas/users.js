bb8e446830efe01f3869bd85130c810eae1819bc
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-01-28 17:58:36 +0100

    Add a refined schema for password validation

--- schemas/users.js
+++ schemas/users.js
@@ -1,10 +1,26 @@
 import z from 'zod'
 
+const passwordSchema = z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long.' })
+    .refine(password => /[A-Z]/.test(password), {
+        message: 'Password must contain an uppercase letter.'
+    })
+    .refine(password => /[a-z]/.test(password), {
+        message: 'Password must contain a lowercase letter.'
+    })
+    .refine(password => /[0-9]/.test(password), {
+        message: 'Password must contain a number.'
+    })
+    .refine(password => /[!@#$%^&*]/.test(password), {
+        message: 'Password must contain a symbol.'
+    })
+
 const userSchema = z.object({
 
-    username: z.string().min(3),
-    email: z.string(),
-    password: z.string().min(6)
+    username: z.string().min(3, {message: 'Username must be at least 3 characters long.'}),
+    email: z.email({message: 'Invalid email format'}),
+    password: passwordSchema,
 
 });
 
