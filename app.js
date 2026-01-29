8ba6d8b49dde80f135d8409543cb3ac663b8e5ef
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-01-30 00:15:56 +0100

    Allow express to serve public files required to render the web page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "node:path";
 import { createViewRouter } from "./routes/view.routes.js";
 import { createAuthRouter } from "./routes/auth.routes.js";
 import { userModel } from "./models/user.model.js";
@@ -8,7 +9,10 @@ export const createApp = () => {
     const app = express();
 
     app.disable('x-powered-by');
+    // Express middleware to use JSON files natively
     app.use(express.json());
+    // Express middleware to serve files from a folder
+    app.use(express.static(path.join(path.resolve(), 'public')));
 
     app.use('/', createViewRouter());
     app.use('/api/auth', createAuthRouter({ userModel }));
