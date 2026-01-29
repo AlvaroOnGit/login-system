7f7fc2ff342372de6d5f6d35e98442a93e1174be
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-01-29 20:38:45 +0100

    Created file to handle authentification routing

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+// Handles routing for authentication (User login/logout/register)
+import { Router } from 'express';
+import { AuthController } from '../controllers/auth.controller.js';
+
+export const createAuthRouter = ({ userModel }) => {
+
+    const authRouter = Router();
+    const authController = new AuthController({ userModel });
+
+    //Declare endpoints
+    authRouter.post('/login', authController.logUser);
+    authRouter.post('/register', authController.createUser);
+    //authRouter.post('/logout', (req, res) => {})
+
+    return authRouter;
+}
