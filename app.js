76c3a9bb82f89bcc89b0ed0cf7ed8f15fa8e49d3
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-02-02 22:27:19 +0100

    Added required middlewares to work with jwt and cookies

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import path from "node:path";
 import { createViewRouter } from "./routes/view.routes.js";
 import { createAuthRouter } from "./routes/auth.routes.js";
+import { accessTokenMiddleware } from "./middlewares/accessToken.middleware.js";
 import { userModel } from "./models/user.model.js";
 
 export const createApp = () => {
@@ -11,8 +13,12 @@ export const createApp = () => {
     app.disable('x-powered-by');
     // Express middleware to use JSON files natively
     app.use(express.json());
+    // Middleware to parse cookies
+    app.use(cookieParser());
     // Express middleware to serve files from a folder
     app.use(express.static(path.join(path.resolve(), 'public')));
+    // Middleware to append user information to each request
+    app.use('/', accessTokenMiddleware);
 
     app.use('/', createViewRouter());
     app.use('/api/auth', createAuthRouter({ userModel }));
