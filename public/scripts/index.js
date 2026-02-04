f33a5937a4baec0b4d65b7cae91695d31766d21f
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-02-04 18:34:14 +0100

    Pruned redundant code

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,42 +1,5 @@
-const logoutButton = document.getElementById("logoutBtn");
+const profileButton = document.getElementById("profileBtn");
 
-let userLogged = false;
-
-document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const res = await fetch("/api/auth/session", {
-            credentials: "include",
-        });
-
-        if (!res.ok) {
-            return;
-        }
-
-        userLogged = true;
-
-        const userData = await res.json();
-
-        document.getElementById("webTitle").textContent = `Hola, ${userData.user.username}!`;
-        document.getElementById("loginBtn").hidden = true;
-        logoutButton.hidden = false;
-    }
-    catch (e) {
-        console.error(e)
-    }
-})
-
-logoutButton.addEventListener("click", async () => {
-
-    try {
-        const res = await fetch("/api/auth/logout", {
-            method: "POST",
-        })
-        if (!res.ok) {
-            console.error('Internal server error');
-        }
-        window.location = '/auth';
-    }
-    catch (e) {
-        console.error(e)
-    }
+profileButton.addEventListener("click", async () => {
+    window.location = '/protected';
 })
