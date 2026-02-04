606bcd2990c88c1488740cc9fc5b9c4d33a926e0
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-02-04 18:36:27 +0100

    Added protected route and logic to redirect users from login to index if they're already logged

--- controllers/view.controller.js
+++ controllers/view.controller.js
@@ -6,6 +6,21 @@ export class ViewController {
         res.sendFile(path.join(path.resolve(), 'views', 'index.html'));
     }
     login = (req, res) => {
+
+        const { user } = req.session;
+
+        if (user) {
+            res.redirect('/');
+        }
+
         res.sendFile(path.join(path.resolve(), 'views', 'auth.html'));
+    }
+    protected = (req, res) => {
+
+        const { user } = req.session;
+
+        if (!user) res.status(403).send('<h1>Not Authorized</h1>');
+
+        res.sendFile(path.join(path.resolve(), 'views', 'protected.html'));
     }
 }
